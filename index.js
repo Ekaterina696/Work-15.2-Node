946d437f1fdd7845c28423d7920b1b2e87a13c98
Ekaterina696 <katya.osacha@mail.ru>
2024-06-06 18:47:10 +0300

    Добавила обработчик произвольного маршрута

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
+//const axios = require('axios');
+//const fs = require('fs');
+//const path = require('path');
 const http = require('http');
 
 
-;(async () => {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+//;(async () => {
+    //const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
     
-    fs.writeFile(path.resolve(__dirname, '1.json'), JSON.stringify(data), 'utf-8', (err) => {
-        if (err) {
-            throw err;
-        }
+    //fs.writeFile(path.resolve(__dirname, '1.json'), JSON.stringify(data), 'utf-8', (err) => {
+        //if (err) {
+           // throw err;
+        //}
 
-        console.log(`Done`);
-    })
-}) ();
+        //console.log(`Done`);
+    //})
+//}) ();
 
 http
     .createServer((request, response) => {
-        response.write('Hello');
+        response.setHeader('Content-Type', 'text/html; charset=utf-8;');
+
+        if (request.url === '/users') {
+            response.write('Список пользователей');
+        } else if (request.url === '/posts') {
+            response.write('Список постов');
+        } else if (request.url === '/users-posts') {
+            response.write('Список постов пользователей'); 
+        }
+        
         response.end();
     })
     .listen(3000);
